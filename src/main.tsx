f33d2767e4635c6e44c0aea796385a115f4419cc
mintuchel <max3627@naver.com>
2024-09-17 02:07:31 +0900

    refactor: for routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App' // 루트 컴포넌트 불러오기
+import App from './App'
 import './index.css' // 전역 스타일 파일 불러오기
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
