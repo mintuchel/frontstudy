30e525172ff3ffd094bcc55d3e4cf3946994848d
mintuchel <max3627@naver.com>
2024-09-18 20:22:14 +0900

    refactor: login

--- src/page/login.tsx
+++ src/page/login.tsx
@@ -14,37 +14,36 @@ const Login = () => {
             <main className="flex-1 p-4 bg-black flex justify-center items-center">
                 <form className="space-y-4">
                     <div className="flex items-center space-x-4">
-                        <label htmlFor="name" className="w-1/3 text-white">ID</label>
                         <input
                             type="text"
                             id="name"
                             name="name"
-                            placeholder="enter id"
-                            className="w-64 p-2 border border-gray-300 rounded"
+                            placeholder="name"
+                            className="w-80 p-2 border border-gray-300 rounded"
                         />
                     </div>
                     <div className="flex items-center space-x-4">
-                        <label htmlFor="nickname" className="w-1/3 text-white">PASSWORD</label>
                         <input
                             type="text"
-                            id="nickname"
-                            name="nickname"
-                            placeholder="enter password"
-                            className="w-64 p-2 border border-gray-300 rounded"
+                            id="password"
+                            name="password"
+                            placeholder="password"
+                            className="w-full p-2 border border-gray-300 rounded"
                         />
                     </div>
+
                     <button
                         type="submit"
                         className="w-full mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
                     >
                         Login
                     </button>
-                    <button
-                        type="submit"
-                        className="w-full mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+                    <Link
+                        to="/signup"
+                        className="w-full block mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 text-center"
                     >
-                        <Link to="/signup" className="text-black">Are you new here? join us!</Link>
-                    </button>
+                        Are you new here? Join us!
+                    </Link>
                 </form>
             </main>
         </section >
